33d93ef484532f8ac5c89a127474fde4a6cf1517
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-19 17:42:26 +0000

    initialize articles as empty list and return it whether hostname/API request worked or not

--- nlp_services/wikia_utils/__init__.py
+++ nlp_services/wikia_utils/__init__.py
@@ -18,9 +18,9 @@ def get_top_articles(wiki_id, n=50):
     :param wiki_id: string, ID of the wiki in Solr
     :param n: int, number of articles to return
     :return: list of article ID strings"""
+    articles = []
     hostname = hostnames.get(wiki_id, 0)
     if hostname:
         items = requests.get('http://' + hostname + '/api/v1/Articles/List?limit=%d' % n).json()
         articles = ['%s_%i' % (wiki_id, item.get('id')) for item in items.get('items', [])]
-        return articles
-    return []
+    return articles
