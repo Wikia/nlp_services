497c70fa96d364baebd83023d5b86fda799db70e
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-19 00:48:46 +0000

    rewrote TopArticlesService as get_top_articles

--- nlp_services/wikia_utils/__init__.py
+++ nlp_services/wikia_utils/__init__.py
@@ -13,19 +13,14 @@ BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 with open(os.path.join(BASE_PATH, 'data/hostnames.json')) as f:
     hostnames = json.loads(f.read())
 
-class TopArticlesService(RestfulResource):
-    """Get top n articles for a given wiki"""
-
-    def __init__(self, n=50):
-        """Constructor; optionally define how many articles to return"""
-        self.n = n
-
-    def get(self, wiki_id):
-        """Return full IDs of the top n articles for a given wiki ID
-        :param wiki_id: string, ID of the wiki in Solr"""
-        hostname = hostnames.get(wiki_id, 0)
-        if hostname:
-            items = requests.get('http://' + hostname + '/api/v1/Articles/List?limit=%d' % self.n).json()
-            articles = ['%s_%i' % (wiki_id, item.get('id')) for item in items.get('items', [])]
-            return articles
-        return []
+def get_top_articles(wiki_id, n=50):
+    """Get top n articles for a given wiki ID
+    :param wiki_id: string, ID of the wiki in Solr
+    :param n: int, number of articles to return
+    :return: list of article ID strings"""
+    hostname = hostnames.get(wiki_id, 0)
+    if hostname:
+        items = requests.get('http://' + hostname + '/api/v1/Articles/List?limit=%d' % n).json()
+        articles = ['%s_%i' % (wiki_id, item.get('id')) for item in items.get('items', [])]
+        return articles
+    return []
