99681d2ea99d064b6a1f1de36ea4acb56b3e112a
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-19 00:16:55 +0000

    include wikia_utils in packages and hostnames.json in data_files

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     license="Other",
     packages=["nlp_services", "nlp_services.caching", "nlp_services.discourse",
               "nlp_services.document_access", "nlp_services.syntax",
-              "nlp_services.title_confirmation", "nlp_services.authority", "nlp_services.test"],
+              "nlp_services.title_confirmation", "nlp_services.authority",
+              "nlp_services.test", "nlp_services.wikia_utils"],
     install_requires=["boto", "flask", "flask-restful", "numpy", "nltk", "lxml",
                       "phpserialize", "mock", "corenlp_xml>=0.0.1", "mrg_utils>=0.0.1"],
     dependency_links=["https://github.com/relwell/corenlp-xml-lib/archive/master.zip#egg=corenlp_xml-0.0.1",
                       "https://github.com/relwell/mrg_utils/archive/master.zip#egg=mrg_utils-0.0.1"
                       ],
+    data_files = [("nlp_services/data", ["nlp_services/data/hostnames.json"])],
     test_suite="nlp_services.test.suite"
     )
