961bb68a6fe42085ac7080a67c346407191061b1
Robert Elwell <robert@wikia-inc.com>
2014-01-28 15:23:44 -0800

    stupid setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     description = "A collection of services for interacting with NLP data extracted from Stanford CoreNLP",
     license = "Other",
     packages = ["nlp_services", "nlp_services.caching", "nlp_services.discourse",
-                "nlp_services.document_access", "nlp_services.syntax", "nlp_services.title_confirmation"],
+                "nlp_services.document_access", "nlp_services.syntax",
+                "nlp_services.title_confirmation", "nlp_services.authority"],
     depends = ["corenlp_xml", "flask", "flask-restful", "boto", "mrg_utils"]
     )
