ee80c7572465f189bb9003a493daeb676b4d6e4b
Tristan Euan Chong <tristaneuan@gmail.com>
2014-02-06 23:41:26 +0000

    removed json dump of hostnames and used Wikis/Details API in wikia_utils.get_top_articles

--- nlp_services/wikia_utils/__init__.py
+++ nlp_services/wikia_utils/__init__.py
@@ -4,14 +4,6 @@ This module contains various Wikia utilities.
 import json
 import os
 import requests
-from .. import RestfulResource
-
-# Get absolute path
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-
-# Load serialized hostnames into memory
-with open(os.path.join(BASE_PATH, 'data/hostnames.json')) as f:
-    hostnames = json.loads(f.read())
 
 def get_top_articles(wiki_id, n=50):
     """Get top n articles for a given wiki ID
@@ -19,8 +11,8 @@ def get_top_articles(wiki_id, n=50):
     :param n: int, number of articles to return
     :return: list of article ID strings"""
     articles = []
-    hostname = hostnames.get(wiki_id, 0)
+    hostname = requests.get('http://www.wikia.com/api/v1/Wikis/Details', params={'ids': wiki_id}).json().get('items', {}).get(wiki_id, {}).get('url')
     if hostname:
-        items = requests.get('http://' + hostname + '/api/v1/Articles/List?limit=%d' % n).json()
+        items = requests.get(hostname + 'api/v1/Articles/List?limit=%d' % n).json()
         articles = ['%s_%i' % (wiki_id, item.get('id')) for item in items.get('items', [])]
     return articles
