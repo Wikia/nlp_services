91397c946ed995e67c5d449982ca377234dab2f9
Tristan Chong <tristanchong@gmail.com>
2014-05-09 17:51:01 +0000

    fix imports, truncate lines

--- nlp_services/title_confirmation/__init__.py
+++ nlp_services/title_confirmation/__init__.py
@@ -1,5 +1,6 @@
 """
-This submodule is used for referencing strings against data sources that connote entities
+This submodule is used for referencing strings against data sources that
+connote entities
 """
 
 from nltk.corpus import stopwords
@@ -21,16 +22,21 @@ def preprocess(title):
     return ' '.join(
         filter(lambda x: x not in stops,
                #500 chars should be plenty, todo fix unicode shit
-               re.sub(' \(\w+\)', '', title.lower().replace('_', ' ')).split(' ')))[:500]
+               re.sub(
+                   ' \(\w+\)', '',
+                   title.lower().replace('_', ' ')).split(' ')))[:500]
 
-from wikia import AllTitlesService
+from wikia import AllTitlesService, RedirectsService
 from wikipedia import check_wp
+
 
 def confirm(title, wiki_id=None, use_wikipedia=None):
     """
     Uses one or more methods for determining whether the title is an entity
-    An entity is defined here as being germane to the scope of the universe of discourse
-    The universe of discourse here is either wikipedia titles or titles in a given wiki
+    An entity is defined here as being germane to the scope of the universe of
+    discourse
+    The universe of discourse here is either wikipedia titles or titles in a
+    given wiki
     :param title: the candidate entity
     :type title: str
     :param use_wikipedia: whether to reference a wiki
@@ -40,9 +46,10 @@ def confirm(title, wiki_id=None, use_wikipedia=None):
     use_wikipedia = USE_WIKIPEDIA if use_wikipedia is None else use_wikipedia
     can_confirm = False
     if wiki_id is not None:
-        can_confirm = preprocess(title) in wikia.AllTitlesService().get_value(wiki_id, [])
+        can_confirm = preprocess(title) in AllTitlesService().get_value(
+            wiki_id, [])
     if not can_confirm and use_wikipedia:
-        can_confirm = wikipedia.check_wp(title)
+        can_confirm = check_wp(title)
     return can_confirm
 
 
@@ -56,5 +63,5 @@ def canonical(title, wiki_id):
     :return: The canonical title
     :rtype: str
     """
-    redirects = wikia.RedirectsService().get_value(wiki_id)
+    redirects = RedirectsService().get_value(wiki_id)
     return redirects.get(preprocess(title), title)
