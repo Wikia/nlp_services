1f03f94cbda71f71dbbae8c10736c664662dd746
Robert Elwell <robert@wikia-inc.com>
2014-06-04 14:09:46 -0700

    added url to setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email="robert.elwell@gmail.com",
     description="A collection of services for interacting with NLP data extracted from Stanford CoreNLP",
     license="Other",
+    url="https://github.com/relwell/nlp_services",
     packages=["nlp_services", "nlp_services.caching", "nlp_services.discourse",
               "nlp_services.document_access", "nlp_services.syntax", "nlp_services.pooling",
               "nlp_services.title_confirmation", "nlp_services.authority", "nlp_services.scripts",
