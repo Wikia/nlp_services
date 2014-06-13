9bd085e50d15a653ca2d8c3dc5f6e4fade4da399
Robert Elwell <robert@wikia-inc.com>
2014-06-13 10:08:00 -0700

    added documentation for discourse entities

--- nlp_services/discourse/entities.py
+++ nlp_services/discourse/entities.py
@@ -22,7 +22,12 @@ class CoreferenceCountsService(RestfulResource):
     @cached_service_request
     def get(self, doc_id):
         """ Returns coreference and mentions for a document
+
         :param doc_id: the id of the document in Solr
+        :type doc_id: str
+
+        :return: a dictionary of mentions and coreferents for the document
+        :rtype: dict
         """
 
         document = document_access.get_document_by_id(doc_id)
@@ -57,14 +62,17 @@ class BaseEntitiesService(RestfulResource):
     @cached_service_request
     def get(self, doc_id):
         """
-        Use title_confirmation module to make this fast on a per-wiki basis
+        Returns noun phrases cross-referenced with a set of titles considered "entities"
+        Depending on the inheriting class, these data sources include a mix of Wikipedia titles and Wikia titles
+
         :param doc_id: the id of the document
         :type doc_id: str
+
         :return: a response keying doc id to redirects and titles
         :rtype: dict
+
         """
 
-        print doc_id
         if not self._use_wikia and not self._use_wikipedia:
             raise Exception("Entity sources aren't configured")
 
@@ -95,16 +103,25 @@ class BaseEntitiesService(RestfulResource):
 
 
 class EntitiesService(BaseEntitiesService):
+    """
+    Cross-references noun phrases with Wikia titles
+    """
     _use_wikia = True
     _use_wikipedia = False
 
 
 class WpEntitiesService(BaseEntitiesService):
+    """
+    Cross-references noun phrases with Wikipedia titles
+    """
     _use_wikia = False
     _use_wikipedia = True
 
 
 class CombinedEntitiesService(BaseEntitiesService):
+    """
+    Cross-references noun phrases with both Wikia and Wikipedia titles
+    """
     _use_wikia = True
     _use_wikipedia = True
 
@@ -120,9 +137,11 @@ class BaseEntityCountsService(RestfulResource):
     def get(self, doc_id):
         """
         Given a doc id, accesses entities and then cross-references entity parses
+
         :param doc_id: the id of the article
         :type doc_id: str
-        :return: respones
+
+        :return: service response, with counts for each entity in the document
         :rtype: dict
         """
         entitiesresponse = self._entity_class().get_value(doc_id, {})
@@ -155,14 +174,23 @@ class BaseEntityCountsService(RestfulResource):
 
 
 class EntityCountsService(BaseEntityCountsService):
+    """
+    Counts only Wikia entities
+    """
     _entity_class = EntitiesService
 
 
 class WpEntityCountsService(BaseEntityCountsService):
+    """
+    Counts Wikipedia entities
+    """
     _entity_class = WpEntitiesService
 
 
 class CombinedEntityCountsService(BaseEntityCountsService):
+    """
+    Counts both Wikia and Wikipedia entities
+    """
     _entity_class = CombinedEntitiesService
 
 
@@ -174,17 +202,20 @@ class BaseWikiEntitiesService(RestfulResource):
 
     @cached_service_request
     def get(self, wiki_id):
-
         """
         Given a wiki doc id, iterates over all documents available.
         For each noun phrase, we confirm whether there is a matching title.
         We then cross-reference that noun phrase by mention count.
+
         :param wiki_id: the id of the wiki
         :type wiki_id: int|str
+
         :return: response
         :rtype: dict
+
         """
 
+        wiki_id = str(wiki_id)
         page_doc_response = document_access.ListDocIdsService().get(wiki_id)
         if page_doc_response['status'] != 200:
             return page_doc_response
@@ -211,24 +242,48 @@ class BaseWikiEntitiesService(RestfulResource):
 
 
 class WikiEntitiesService(BaseWikiEntitiesService):
+    """
+    Entities counted are only related to Wikia titles
+    """
     _entity_count_service = EntityCountsService
 
 
 class WpWikiEntitiesService(BaseWikiEntitiesService):
+    """
+    Entities counted are only related to Wikipedia titles
+    """
     _entity_count_service = WpEntityCountsService
 
 
 class CombinedWikiEntitiesService(BaseWikiEntitiesService):
+    """
+    Entities counted are related to both Wikia and Wikipedia titles
+    """
     _entity_count_service = CombinedEntityCountsService
 
 
 class BaseTopEntitiesService(RestfulResource):
-    _entities_service = None
     """
     Gets top 50 entities for a given wiki
     """
+
+    _entities_service = None
+
     @cached_service_request
     def get(self, wiki_id):
+        """
+        For a given entities service, configured in child classes, accesses counts for all
+        entities in the wiki, and returns the top 50, sorted by frequency
+
+        :param wiki_id: the ID of the wiki
+        :type wiki_id: str|int
+
+        :return: dictionary keying wiki ID to ordered list of entity to count
+        :rtype: dict
+
+        """
+
+        wiki_id = str(wiki_id)
         counts_to_entities = self._entities_service().get_value(wiki_id, {})
         items = sorted([(val, key) for key in counts_to_entities.keys() for val
                         in counts_to_entities[key]],
@@ -238,32 +293,46 @@ class BaseTopEntitiesService(RestfulResource):
 
 
 class TopEntitiesService(BaseTopEntitiesService):
+    """
+    Gets top Wikia entities only for this wiki
+    """
     _entities_service = WikiEntitiesService
 
 
 class WpTopEntitiesService(BaseTopEntitiesService):
+    """
+    Gets top Wikipedia entities only for this wiki
+    """
     _entities_service = WpWikiEntitiesService
 
 
 class CombinedTopEntitiesService(BaseTopEntitiesService):
+    """
+    Gets top Wikipedia and Wikia entities for this wiki
+    """
     _entities_service = CombinedWikiEntitiesService
 
 
 class BaseWikiPageEntitiesService(RestfulResource):
     """ Aggregates entities over a wiki by page """
     _entity_count_service = None
+
     @cached_service_request
     def get(self, wiki_id):
         """
         Given a wiki doc id, iterates over all documents available.
         For each noun phrase, we confirm whether there is a matching title.
         We then cross-reference that noun phrase by mention count.
+
         :param wiki_id: the id of the wiki
         :type wiki_id: str|int
+
         :rtype: dict
         :return: response
+
         """
 
+        wiki_id = str(wiki_id)
         page_doc_response = document_access.ListDocIdsService().get(wiki_id)
         if page_doc_response['status'] != 200:
             return page_doc_response
@@ -275,14 +344,23 @@ class BaseWikiPageEntitiesService(RestfulResource):
 
 
 class WikiPageEntitiesService(BaseWikiPageEntitiesService):
+    """
+    Provides Wikia entity counts per page
+    """
     _entity_count_service = EntityCountsService
 
 
 class WpWikiPageEntitiesService(BaseWikiPageEntitiesService):
+    """
+    Provides Wikipedia entity counts per page
+    """
     _entity_count_service = WpEntityCountsService
 
 
 class CombinedWikiPageEntitiesService(BaseWikiPageEntitiesService):
+    """
+    Provides Wikia and Wikipedia entity counts per page
+    """
     _entity_count_service = CombinedEntityCountsService
 
 
@@ -295,12 +373,16 @@ class BaseDocumentCountsService(RestfulResource):
         """ Given a wiki doc id, iterates over all documents available.
         For each noun phrase, we confirm whether there is a matching title.
         We then cross-reference that noun phrase by document count, not mention count
+
         :param wiki_id: the id of the wiki
         :type wiki_id: int|str
+
         :rtype: dict
         :return: response
+
         """
 
+        wiki_id = str(wiki_id)
         page_doc_response = document_access.ListDocIdsService().get(wiki_id)
         if page_doc_response['status'] != 200:
             return page_doc_response
@@ -326,20 +408,41 @@ class BaseDocumentCountsService(RestfulResource):
 
 
 class EntityDocumentCountsService(BaseDocumentCountsService):
+    """
+    Returns number of documents each Wikia entity appears in
+    """
     _entity_count_service = EntityCountsService
 
 
 class WpEntityDocumentCountsService(BaseDocumentCountsService):
+    """
+    Returns number of documents each Wikipedia entity appears in
+    """
     _entity_count_service = WpEntityCountsService
 
 
 class CombinedDocumentEntityCountsService(BaseDocumentCountsService):
+    """
+    Returns number of documents each Wikia or Wikipedia entity appears in
+    """
     _entity_count_service = CombinedEntityCountsService
 
 
 class BaseWikiPageToEntitiesService(RestfulResource):
+    """
+    Base service to define behavior for getting entities for all pages in a wiki.
+    """
+    def map_pageids(self, doc_ids):
+        """
+        This just defines a common API for child classes.
+        If you try to run this you get nothing, Lebowski.
 
-    def map_pageids(self, pageids):
+        :param doc_ids: list of doc ids
+        :type doc_ids: list
+
+        :return: None
+        :rtype: None
+        """
         return None
 
     @cached_service_request
@@ -347,12 +450,16 @@ class BaseWikiPageToEntitiesService(RestfulResource):
         """ Given a wiki doc id, iterates over all documents available.
         The response is a dictionary that keys documents to entities.
         This is mostly for caching!
+
         :param wiki_id: the id of the wiki
         :type wiki_id: int|str
+
         :rtype: dict
         :return: response
+
         """
 
+        wiki_id = str(wiki_id)
         page_doc_response = document_access.ListDocIdsService().get(wiki_id)
         if page_doc_response['status'] != 200:
             return page_doc_response
@@ -363,28 +470,97 @@ class BaseWikiPageToEntitiesService(RestfulResource):
         return {wiki_id: dict(zip(page_doc_ids, self.map_pageids(page_doc_ids))), 'status': 200}
 
 
-def es_get(pageid):
-    return EntitiesService().get_value(pageid)
+def es_get(doc_id):
+    """
+    Gets the EntitiesService response for a document ID. Used in multiprocessing.
+
+    :param doc_id: the document ID
+    :type doc_id: str
+
+    :return: entities service response
+    :rtype: dict
+
+    """
+    return EntitiesService().get_value(doc_id)
 
 
 class WikiPageToEntitiesService(BaseWikiPageToEntitiesService):
-    def map_pageids(self, pageids):
-        return pool(num_processes=32).map_async(es_get, pageids).get()
+    """
+    Gets wikia entities for each page -- useful for caching
+    """
+    def map_pageids(self, doc_ids):
+        """
+        Uses multiprocessing to get wikia entities for all documents
+
+        :param doc_ids: list of document ids
+        :type doc_ids: list
+
+        :return: a list of response dicts
+        :rtype: list
+
+        """
+        return pool(with_max=True).map_async(es_get, doc_ids).get()
 
 
-def wpes_get(pageid):
-    return WpEntitiesService().get_value(pageid)
+def wpes_get(doc_id):
+    """
+    Gets the WpEntitiesService response for a document ID. Used in multiprocessing.
+
+    :param doc_id: the document ID
+    :type doc_id: str
+
+    :return: entities service response
+    :rtype: dict
+
+    """
+    return WpEntitiesService().get_value(doc_id)
 
 
 class WpPageToEntitiesService(BaseWikiPageToEntitiesService):
-    def map_pageids(self, pageids):
-        print pool(with_max=True).map_async(wpes_get, pageids).get()
+    """
+    Gets wikipedia entities for each page -- useful for caching
+    """
+    def map_pageids(self, doc_ids):
+        """
+        Uses multiprocessing to get wikipedia entities for all documents
+
+        :param doc_ids: list of document ids
+        :type doc_ids: list
+
+        :return: a list of response dicts
+        :rtype: list
+
+        """
+        print pool(with_max=True).map_async(wpes_get, doc_ids).get()
 
 
-def ces_get(pageid):
-    return CombinedEntitiesService.get_value(pageid)
+def ces_get(doc_id):
+    """
+    Gets the CombinedEntitiesService response for a document ID. Used in multiprocessing.
+
+    :param doc_id: the document ID
+    :type doc_id: str
+
+    :return: entities service response
+    :rtype: dict
+
+    """
+    return CombinedEntitiesService().get_value(doc_id)
 
 
 class CombinedPageToEntitiesService(BaseWikiPageToEntitiesService):
-    def map_pageids(self, pageids):
-        print pool(with_max=True).map_async(ces_get, pageids).get()
+    """
+    Gets wikia and wikipedia entities for each page -- useful for caching
+    """
+    def map_pageids(self, doc_ids):
+        """
+        Uses multiprocessing to get wikipedia and wikia entities for all documents
+
+        :param doc_ids: list of document ids
+        :type doc_ids: list
+
+        :return: a list of response dicts
+        :rtype: list
+
+        """
+        print pool(with_max=True).map_async(ces_get, doc_ids).get()
