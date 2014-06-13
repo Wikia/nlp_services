89ed9007a2973b0fa027d3387aee852e271cf674
Robert Elwell <robert@wikia-inc.com>
2014-06-13 13:29:19 -0700

    final bit of module documentation -- api stuff is next

--- nlp_services/__init__.py
+++ nlp_services/__init__.py
@@ -12,9 +12,14 @@ class RestfulResource(restful.Resource):
     def get_value(self, doc_id, backoff=None):
         """
         Allows us to call a service and extract data from its response
+
         :param doc_id: the id of the document
+        :type doc_id: str
         :param backoff: default value
+        :type backoff: mixed
+
         :return: whatever the return value of the method call is
+        :rtype: mixed
         """
         return self.get(doc_id).get(doc_id, backoff)
 
